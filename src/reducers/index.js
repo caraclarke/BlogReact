51670984134249bbc2394595b5b13bc583a219c9
caraclarke <cara.clarke8@gmail.com>
2016-07-19 13:38:43 -0400

    add redux-form and set up reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import PostsReducer from './post_reducer';
+import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
-  post: PostsReducer
+  post: PostsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
