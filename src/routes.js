04a725aa64ffa1c5dd6895de8d613aac506a0c50
caraclarke <cara.clarke8@gmail.com>
2016-07-19 11:42:00 -0400

    add new component and index route

--- src/routes.js
+++ src/routes.js
@@ -5,9 +5,11 @@ Route object is what we use to define a match between URL and component
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
+import PostIndex from './components/PostIndex';
 
 // matches path of / to App component
 export default (
   <Route path="/" component={App}>
+    <IndexRoute component={PostIndex} />
   </Route>
 );
