d0d14175f8c11dd491ad28f3e1219df635191e22
caraclarke <cara.clarke8@gmail.com>
2016-07-19 13:30:02 -0400

    added component and route for new post

---
+++ src/components/PostNew.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PostNew extends Component {
+  render() {
+    return (
+      <div>create form</div>
+    );
+  }
+}
+
+export default PostNew;
--- src/routes.js
+++ src/routes.js
@@ -8,10 +8,12 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
 import PostIndex from './components/PostIndex';
+import PostNew from './components/PostNew';
 
 // matches path of / to App component
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={PostIndex} />
+    <Route path="posts/new" component={PostNew} />
   </Route>
 );
