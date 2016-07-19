4a1065567872b03b329b72cdac4be1a555dc8663
caraclarke <cara.clarke8@gmail.com>
2016-07-19 12:22:14 -0400

    making request, request 200

--- src/components/PostIndex.js
+++ src/components/PostIndex.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
 
 class PostIndex extends Component {
 
   componentWillMount() {
-    console.log('this would be blah');
+    this.props.fetchPosts();
   }
 
   render() {
@@ -13,4 +16,17 @@ class PostIndex extends Component {
   }
 }
 
-export default PostIndex;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchPosts }, dispatch);
+}
+
+// null because dont have mapStateToProps which is usually first argument because dont have state we want to map over yet
+export default connect(null, mapDispatchToProps)(PostIndex);
+
+/* **steps to create container that can call action creators**
+ 1. import connect at top
+ 2. import action creator
+ 3. define mapDispatchToProps
+ 4. connect to component
+
+*/
